3c1c109b6ce1bb013fa693ffd2ed8adfa31326ea
Vanessa Trinh <xtourmaline@gmail.com>
2025-04-21 00:57:31 -0400

    force dynamic

--- t3gallery/src/app/page.tsx
+++ t3gallery/src/app/page.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import Image from 'next/image'
 import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic"
 
 const mockUrls = [
   "https://rad0fe6bke.ufs.sh/f/iV4qqqZQGaz7G9ANDEJA2EovO1HBin8WmXUZhzY35fp6q7gN", "https://rad0fe6bke.ufs.sh/f/iV4qqqZQGaz7BBWIsxaYw0st8xoi9VafdGJZ6l7eqnKN24OL", "https://rad0fe6bke.ufs.sh/f/iV4qqqZQGaz7Q5mr86BBwFpGAf2ziLKy9NXJVhYc0lkrIMeq", "https://rad0fe6bke.ufs.sh/f/iV4qqqZQGaz7JXDecy0tq5Fr6CvAMlnQechyNHXIaujBJk9w"
