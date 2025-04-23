ddafd0b369d8ecdf875f9b419c44e70d7486eb29
Vanessa Trinh <xtourmaline@gmail.com>
2025-04-23 04:28:25 -0400

    Custom upload button

--- t3gallery/src/app/_components/topnav.tsx
+++ t3gallery/src/app/_components/topnav.tsx
@@ -1,24 +1,17 @@
-"use client";
-
 import { SignedIn, SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export function TopNav() {
-  const router = useRouter();
-
   return (
     <nav className="flex items-center justify-between w-full p-4 text-xl font-xl font-semibold border-b">
       <div>Gallery</div>
   
-      <div className="flex flex-row">
+      <div className="flex flex-row gap-4 items-center">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton endpoint="imageUploader" onClientUploadComplete={() => {
-            router.refresh();
-          }}/>
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
--- t3gallery/src/components/full-image-page.tsx
+++ t3gallery/src/components/full-image-page.tsx
@@ -12,7 +12,7 @@ export default async function FullPageImageView(props: { id: number }) {
                 <img src={image.url} className="flex-shrink object-contain" />
             </div>
 
-            <div className="flex w-48 flex-shrink-0 flex-col border-l">
+            <div className="flex w-48 flex-shrink-0 flex-col border-l"> 
                 <div className="text-lg p-2 border-b text-center">{image.name}</div>
 
                 <div className="flex flex-col p-2">
