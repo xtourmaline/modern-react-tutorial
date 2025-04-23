3e14dc8640c0823e05778569f63a7a0cc034b58e
Vanessa Trinh <xtourmaline@gmail.com>
2025-04-23 04:52:24 -0400

    Add styling and loading spinner for uploading state

--- t3gallery/src/app/_components/simple-upload-button.tsx
+++ t3gallery/src/app/_components/simple-upload-button.tsx
@@ -32,20 +32,35 @@ const useUploadThingInputProps = (...args: Input) => {
 
 function UploadSVG() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+        </svg>
+    );
+}
+
+function LoadingSpinnerSVG() {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white">
+            <path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25" />
+            <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" className="spinner_ajPY"/>
         </svg>
     );
 }
 
 export function SimpleUploadButton() {
     const router = useRouter();
+
     const { inputProps } = useUploadThingInputProps("imageUploader", {
         onUploadBegin() {
-            toast("Uploading...", {
-                duration: 100000,
-                id: "upload-begin",
-            });
+            toast(
+                <div className="flex gap-2 text-white items-center">
+                    <LoadingSpinnerSVG /> <span className="text-lg">Uploading...</span>
+                </div>,
+                {
+                    duration: 100000,
+                    id: "upload-begin",
+                },
+            );
         },
         onClientUploadComplete() {
             toast.dismiss("upload-begin");
