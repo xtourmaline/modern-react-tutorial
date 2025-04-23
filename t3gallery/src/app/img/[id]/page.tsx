befa425d97a1214b05fd5904002b656d32c64628
Vanessa Trinh <xtourmaline@gmail.com>
2025-04-23 04:00:39 -0400

    move to grid

--- t3gallery/src/app/img/[id]/page.tsx
+++ t3gallery/src/app/img/[id]/page.tsx
@@ -8,5 +8,9 @@ export default function PhotoPage({
     const idAsNumber = Number(photoId);
     if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
 
-    return <FullPageImageView id={idAsNumber} />;
+    return (
+        <div className="h-full">
+            <FullPageImageView id={idAsNumber} />
+        </div>
+    );
 }
